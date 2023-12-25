71ea383684c1627318c16835b4c1cec7bf3f6368
Nipun-9420 <71821115+Nipun-9420@users.noreply.github.com>
2023-12-25 07:21:02 +0530

    TIME PASS

--- Simple_counter_switch_fpga.v
+++ Simple_counter_switch_fpga.v
@@ -1,7 +1,16 @@
+//controll display with switch and count it down to 0
 module Simple_counter_switch_fpga(
 								input clk,
 								output reg [0:7]d	
 												);
+		reg clk_1=0;
+		integer count_1;
+		always @(posedge clk) begin
+			if(count_1 == 25000000)
+				clk_1 = ~ clk_1;
+			else
+
+		end
 
 endmodule 
 
